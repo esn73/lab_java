c6df8bd154f3ace8bd2b7141fa2fc2078819ce9a
esn73 <esn7338@gmail.com>
2022-01-13 13:38:56 +0900

    .

---
+++ Lab00_Contact/src/edu/java/contact/ver03/MainMenu.java
@@ -0,0 +1,10 @@
+package edu.java.contact.ver03;
+
+public interface MainMenu {
+	int QUIT = 0; // 종료
+	int SELECT_ALL = 1; // 전체검색
+	int SELECT_BY_INDEX = 2; // 인덱스로 연락처 1개 정보 검색
+	int INSERT = 3; // 추가
+	int UPDATE = 4; // 수정
+	int DELETE = 5; // 삭제
+}
