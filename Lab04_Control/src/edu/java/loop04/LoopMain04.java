34519122009318763d2b786da4aab49bface51e8
esn73 <esn7338@gmail.com>
2022-02-05 23:09:59 +0900

    .

---
+++ Lab04_Control/src/edu/java/loop04/LoopMain04.java
@@ -0,0 +1,16 @@
+package edu.java.loop04;
+
+public class LoopMain04 {
+	public static void main(String[] args) {
+		/* 구구단 2단을 아래와 같은 형식으로 출력
+		   2 x 1 = 2
+		   2 x 2 = 4
+		   2 x 3 = 6
+		   ...
+		   2 x 9 = 18
+		 */
+		
+		
+	}
+
+}
